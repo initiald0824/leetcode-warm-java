b2cede64b7fd3b94a3c498935c5a1a4d3dbbcc06
daijun.0824 <daijun.0824@bytedance.com>
2020-01-21 21:50:28 +0800

    *

---
+++ src/CopyFlightBookings.java
@@ -0,0 +1,52 @@
+//这里有 n 个航班，它们分别从 1 到 n 进行编号。
+//
+// 我们这儿有一份航班预订表，表中第 i 条预订记录 bookings[i] = [i, j, k] 意味着我们在从 i 到 j 的每个航班上预订了 k 个座
+//位。
+//
+// 请你返回一个长度为 n 的数组 answer，按航班编号顺序返回每个航班上预订的座位数。
+//
+//
+//
+// 示例：
+//
+// 输入：bookings = [[1,2,10],[2,3,20],[2,5,25]], n = 5
+//输出：[10,55,45,25,25]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= bookings.length <= 20000
+// 1 <= bookings[i][0] <= bookings[i][1] <= n <= 20000
+// 1 <= bookings[i][2] <= 10000
+//
+// Related Topics 数组 数学
+
+import java.util.Arrays;
+
+/**
+ * @author initiald0824
+ * @date 2020/1/5 21:41
+ */
+public class CopyFlightBookings {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] res = new int[n];
+        for (int i = 0; i < bookings.length; i++) {
+            for (int j = bookings[i][0]; j <= bookings[i][1]; j++) {
+                res[j-1] += bookings[i][2];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] bookings = {
+                {1, 2, 10},
+                {2, 3, 20},
+                {2, 5, 25}
+        };
+        System.out.println(Arrays.toString(new CopyFlightBookings().corpFlightBookings(bookings, 5)));
+    }
+}
--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -20,27 +20,78 @@
  */
 public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
-        for (int i = 0; i < s.length(); i++) {
-        }
-        return "";
+        return centralSymmetry(s);
     }
 
-    public String getPalindrome(String s, int left, int right) {
-        int cnt = 0, pl = left, pr = right;
-        while (left >=0 && right < s.length()) {
-            if (s.charAt(left) == s.charAt(right)) {
+    /**
+     * 中心对称法
+     * @param s
+     * @return 结果
+     */
+    public String centralSymmetry(String s) {
+        int len = s.length() * 2 - 1;
+        String maxStr = "";
+        for (int i = 0; i < len; i++) {
+            String res;
+            if (i % 2 == 1) {
+                res = centralSymmetryFunc(s, "", (i-1)/2, (i+1)/2);
+            } else {
+                res = centralSymmetryFunc(s, String.valueOf(s.charAt(i/2)), i/2-1, i/2+1);
+            }
+            maxStr = maxStr.length() > res.length() ? maxStr : res;
+        }
+        return maxStr;
+    }
+
+    private String centralSymmetryFunc(String src, String center, int left, int right) {
+        String res = "";
+        while (left >= 0 && right < src.length()) {
+            if (src.charAt(left) == src.charAt(right)) {
+                res = src.substring(left, right+1);
                 left--;
                 right++;
-                cnt++;
             } else {
                 break;
             }
         }
-        return s.substring(pl-cnt, pl) + s.substring(pr, pr+cnt);
+        res = res.length() > center.length() ? res : center;
+        return res;
+    }
+
+
+    /**
+     * 动态规划法
+     * @param s
+     * @return
+     */
+    public String dynamicPlan(String s) {
+        int len = s.length();
+        String res = "";
+        boolean record[][] = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            record[i][i] = true;
+            res = res.length() > s.substring(i, i+1).length() ? res : s.substring(i, i+1);
+            if (i < len -1 && s.charAt(i) == s.charAt(i+1)) {
+                record[i][i+1] = true;
+                res = res.length() > s.substring(i, i+2).length() ? res : s.substring(i, i+2);
+            }
+        }
+
+        for (int step = 2; step < len; step++) {
+            for (int i = 0; i < len-step; i++) {
+                int j = i + step;
+                if (s.charAt(i) == s.charAt(j) && record[i+1][j-1]) {
+                    record[i][j] = true;
+                    res = res.length() > s.substring(i, j+1).length() ? res : s.substring(i, j+1);
+                } else {
+                    record[i][j] = false;
+                }
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        System.out.println(new LongestPalindromicSubstring().getPalindrome("babad", 0, 0));
+        System.out.println(new LongestPalindromicSubstring().dynamicPlan("abcba"));
     }
-
 }
